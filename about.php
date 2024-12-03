7535474c6cfe9d3bc712e18f8fbfb614a798a701
Lucas Winger <lwinger@cord.edu>
2024-12-03 11:09:55 -0600

    Edit Event Works

--- about.php
+++ about.php
@@ -25,11 +25,7 @@ require_once("header.php")
         <h1>The <br /><span class="highlight">Entrepreneurship</span> <br />Club</h1>
     </div>  
     <div class="right-section">
-        <img src="img/contactImg/about.png">
-        <img src = "img/contactImg/flowerPot.png" class = "flowerPot">
-        <img src = "img/contactImg/aboutLeftLeaf.png" class = "leaf1">
-        <img src = "img/contactImg/aboutMiddleLeaf.png" class = "leaf2">
-        <img src = "img/contactImg/aboutRightLeaf.png" class = "leaf3">
+        <img src="img/contactImg/about.png" alt="About the Club">
     </div>
     </div>
 <!---------------------- CONTAINER 1 MEMBERS---------------------->
